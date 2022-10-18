d8beaa3d64f2bd3b8478157a60505ce8e8063bb8
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2022-10-17 22:03:46 -0300

    #1-release: implementando lista de itens no orçamento

--- calcula_imposto.py
+++ calcula_imposto.py
@@ -1,14 +1,21 @@
 class Calcula_impostos(object):
 	def calcula(self,orcamento, imposto):
+		orcamento.set_item(
+			[
+			Item('Item A', 100.0),
+			Item('Item B', 50.0),
+			Item('Item C', 400.0)
+			]
+		)
 		return imposto.calcula(orcamento) # Duck typing
 
 
 if __name__ == "__main__":
-	from orcamento import Orcamento
+	from orcamento import Orcamento, Item
 	import impostos as ipt
 	print(
 			[
-				Calcula_impostos().calcula(Orcamento(500), i)
+				Calcula_impostos().calcula(Orcamento(), i)
 				 for i in ipt.get()
 			]
 		)
--- orcamento.py
+++ orcamento.py
@@ -1,7 +1,44 @@
 class Orcamento(object):
-	def __init__(self, valor):
-		self.__valor = valor # dado "privado"
 
-	@property	# encapsulamento
-	def valor(self):
-		return self.__valor
+    def __init__(self):
+        self.__itens = []
+
+    # quando a propriedade for acessada, ela soma cada item retornando o valor do orçamento
+    @property
+    def valor(self):
+        total = 0.0
+        for item in self.__itens:
+            total+= item.valor
+        return total
+
+    # retornamos uma tupla, já que não faz sentido alterar os itens de um orçamento
+    def obter_itens(self):
+
+        return tuple(self.__itens)
+
+    # perguntamos para o orçamento o total de itens
+    @property
+    def total_itens(self):
+        return len(self.__itens)
+
+    def adiciona_item(self, item):
+        self.__itens.append(item)
+
+    def set_item(self,value):
+        self.__itens = value
+        return Orcamento()
+
+# um item criado não pode ser alterado, suas propriedades são somente leitura
+class Item(object):
+
+    def __init__(self, nome, valor):
+        self.__nome = nome
+        self.__valor = valor
+
+    @property
+    def valor(self):
+        return self.__valor
+
+    @property
+    def nome(self):
+        return self.__nome
